f008b4d525cacb2130725be9d1542e455d8db0f1
mrnlso <46461945+marianoaloiso@users.noreply.github.com>
2024-06-02 17:14:13 -0700

    update methods to PyPDF2 3.0

--- backend/process.py
+++ backend/process.py
@@ -3,22 +3,48 @@ import os
 import PyPDF2
 
 def load_tailor_resume_prompt() -> str:
+    """
+    Load the prompt to tailor a resume.
+    :return: str: The prompt
+    """
     base_path = os.path.dirname(__file__)
-    prompt_path = os.path.join(base_path, "prompts/tailor_resume.txt")
+    prompt_path = os.path.join(
+        base_path,
+        "prompts/tailor_resume.txt"
+        )
     with open(prompt_path, "r") as file:
         prompt = file.read()
     return prompt
 
 def extract_text_from_pdf(file) -> str:
-    reader = PyPDF2.PdfFileReader(file)
+    """
+    Extract text from a PDF file.
+    :param file: str: Path to the PDF file
+    :return: str: Extracted text
+    """
+    reader = PyPDF2.PdfReader(file)
     text = ""
-    for page_num in range(reader.numPages):
-        page = reader.getPage(page_num)
+    for page_num in range(len(reader.pages)):
+        page = reader.pages[page_num]
         text += page.extract_text()
     return text
 
-def process(resume_text: str, job_description: str, gemini_api_key: str) -> str:
+def process(
+        resume_text: str,
+        job_description: str,
+        api_key: str
+    ) -> str:
+    """
+    Process the resume and job description to generate a response.
+    :param resume_text: str: Resume text
+    :param job_description: str: Job description
+    :param api_key: str: API key
+    :return: str: Generated text
+    """
     prompt = load_tailor_resume_prompt()
-    prompt = prompt.format(resume_text=resume_text, job_description=job_description)
-    response = generate_response(prompt, gemini_api_key)
+    prompt = prompt.format(
+        resume_text=resume_text,
+        job_description=job_description)
+    response = generate_response(prompt, api_key)
     return response
+
